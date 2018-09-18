37ed0fbbe93872b18fc675a15a550fcfc5407320
Naila Kaliyeva <nailakaliyeva@gmail.com>
2018-09-18 23:35:02 +0000

    Need to figure out how to display cards inline

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,8 +8,8 @@ export class Home extends React.Component{
             <div>
                 <Navbar />
                 <Greeting />
-                <Card /><Card />
-                
+                <Card style = {{display: "inline"}}/> <Card style = {{display: "inline"}} />
+                <Footer />
                 
                 
             </div>
@@ -36,7 +36,7 @@ function Greeting() {
                 <div style = {{backgroundColor: "#e3e5e8", marginTop: "30px", borderRadius: "10px", padding: "30px"}} className = "col-12">
                     <h1 style = {{fontFamily: '"Open Sans Condensed", sans-serif', fontSize: "75px"}}>A Warm Welcome!</h1>
                     <p style ={{fontSize: "20px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <a href="https://www.youtube.com/watch?v=3YxaaGgTQYM" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </div>
@@ -44,22 +44,32 @@ function Greeting() {
 }
 
 
-function Card() {
+function Card(hrefImage, cardTitle, text, hrefLink) {
     return(
-        <div className = "container">
-            <div className = "row">
-                <div className = "col-3" style ={{marginTop: "30px"}}>
-                    <div className="card" style={{width: "18rem"}}>
-                        <img className="card-img-top" src="https://picsum.photos/500/325" alt="Card image cap"></img>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
+        <div className = "col-3" style ={{marginTop: "30px", marginLeft: "88px", marginBottom: "30px"}}>
+            <div className="card" style={{width: "18rem"}}>
+                <img className="card-img-top" src="https://picsum.photos/500/325"/*{hrefImage}*/ alt="Card image cap"></img>
+                <div className="card-body">
+                    <h5 className="card-title"/*{cardTitle}*/>Card title</h5> 
+                    <p className="card-text"/*{text}*/>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="https://www.youtube.com/watch?v=BQ0mxQXmLsk"/*{hrefLink}*/ className="btn btn-primary">Go somewhere</a>
                 </div>
-            </div>    
+            </div>
         </div>
+        );
+}
+
+function Footer() {
+    return(
+        <div className= "container-fluid">
+            <div className = "row">
+                <div className = "col-12" style = {{backgroundColor: "#3d4244"}}>
+                    <p style = {{color: "white", textAlign: "center", paddingTop: "50px", paddingBottom: "50px"}}>Copyright © Your Website 2018</p>
+                </div>    
+            </div>
+            
+        </div>
+        
         );
 }
 
