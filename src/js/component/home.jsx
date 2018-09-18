cb982324115efec7728afe1f36d1a34f7f25607f
Carlos Rivera <crivera2596@gmail.com>
2018-09-18 00:49:24 +0000

    Starting on Navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
-//include images into your bundle
-import rigoImage from '../../img/rigo-baby.jpg';
-
 //create your first component
 export class Home extends React.Component{
     
     render(){
         return (
-            <div className="text-center mt-5">
-                <h1>Hello Rigo!</h1>
-                <p><img src={rigoImage} /></p>
-                <a href="#" className="btn btn-success">If you see this green button... bootstrap is working</a>
-                <p>Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!</p>
-            </div>
+            <Navbar />
         );
     }
+}
+
+function Navbar() {
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <nav className="navbar navbar-dark bg-dark">
+                    <a className="navbar-brand" href="#">
+                        <h2>Start Bootstrap</h2>
+                    </a>
+                </nav>
+            </div>
+        </div>
+    );
 }
